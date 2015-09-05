b031215ce275e068ceb1df19c5e010c1e4492057
Scott Umsted <scottumsted@gmail.com>
2015-09-05 14:40:59 -0500

    checking for color

--- server/gopi_camera.py
+++ server/gopi_camera.py
@@ -19,6 +19,12 @@ def landing():
 
 
 class GopiImage():
+    COLORS = {
+        'red': (200, 20, 20),
+        'green': (20, 200, 20),
+        'blue': (20, 20, 200)
+    }
+
     IMAGE_SIZE = (320, 240)
     SPOT_MAP = {
         -15: (20, 100, 60, 140),
@@ -115,7 +121,6 @@ class GopiImage():
             line = (spot[0], spot[3], spot[0], spot[1])
             draw.line(line, fill=red, width=3)
 
-
         fp = io.BytesIO()
         image.save(fp, 'PNG')
         image_byte_array = fp.getvalue()
@@ -130,6 +135,27 @@ class GopiImage():
 
     def get_spot(self):
         pass
+
+    def find_color(self, color):
+        image = Image.open(cStringIO.StringIO(self.image_byte_array))
+        image_data = image.getdata()
+        match = None
+        for degrees, spot in self.SPOT_MAP.iteritems():
+            r, g, b = 0, 0, 0
+            for x in range(spot[0], spot[2]):
+                for y in range(spot[1], spot[3]):
+                    r += image_data[x * y][0]
+                    g += image_data[x * y][1]
+                    b += image_data[x * y][2]
+            num_pixels = (spot[2] - spot[0]) * (spot[3] - spot[1])
+            ar = r / num_pixels
+            ag = g / num_pixels
+            ab = b / num_pixels
+            print('%d, (%d, %d, %d)' % (degrees, ar, ag, ab))
+            if ar > self.COLORS[color][0] and ag < self.COLORS[color][1] and ab < self.COLORS[color][2]:
+                match = degrees
+                break
+        return match
 
 
 @get('/cam/target')
@@ -164,8 +190,8 @@ def cam_image(kargs):
     with picamera.PiCamera() as camera:
         camera.resolution = gpi.IMAGE_SIZE
         camera.capture_sequence([gpi], format="jpeg", use_video_port=False)
-
-    return {'image': gpi.get_image_spot_overlay(0)}
+    degrees = gpi.find_color(kargs['color'])
+    return {'image': gpi.get_image_spot_overlay(degrees), 'degrees': degrees}
 
 
 @get('/cam/spot/<degrees>')
@@ -174,18 +200,21 @@ def cam_spot(kargs):
     pass
 
 
-# if __name__ == '__main__':
-#     infile = 'static/images/pi1.jpg'
-#     outfile = 'static/images/wo.html'
-#     iba = open(infile, 'rb').read()
-#     gpi = GopiImage()
-#     gpi.write(iba)
-#     b = gpi.get_image_with_overlay()
-#     with open(outfile, 'w') as of:
-#         of.write('<html><body><img src="data:image/png;base64,' + b + '"></body></html>')
-#     pass
-#
-
 if __name__ == '__main__':
-    debug(debug_mode)
-    run(host='0.0.0.0', port=port, debug=debug_mode)
+    infile = 'static/images/red.jpg'
+    outfile = 'static/images/wo.html'
+    iba = open(infile, 'rb').read()
+    gpi = GopiImage()
+    gpi.write(iba)
+
+    gpi.find_color('red')
+    b = gpi.get_image_spot_overlay()
+
+    with open(outfile, 'w') as of:
+        of.write('<html><body><img src="data:image/png;base64,' + b + '"></body></html>')
+    pass
+
+
+# if __name__ == '__main__':
+#     debug(debug_mode)
+#     run(host='0.0.0.0', port=port, debug=debug_mode)
